1385bbf3b2b34fe9ed3c433d9ea15515c2b4367b
godgel <83522575+godgel@users.noreply.github.com>
2022-01-23 10:13:47 +0900

    initial commit

---
+++ seok_javascript_var_let.js
@@ -0,0 +1,32 @@
+/* var _error$occurs_multi_2 = 10000;
+
+console.log(_error$occurs_multi_2);
+
+var _error$occurs_multi_2 = "10000*200";
+
+console.log(_error$occurs_multi_2); */
+
+/* let _error$occurs_multi_2 = 20000;
+
+console.log(_error$occurs_multi_2); */
+
+let _error$occurs_multi_2 = "20000*3000";
+
+console.log(_error$occurs_multi_2);
+
+let _do_not_use_var_$cause_re_allocate_proble =
+  "Don't Use Var, Use let instead of it.";
+
+console.log(_do_not_use_var_$cause_re_allocate_proble);
+
+/* ====================================================================
+
+if someone use var, and another persone use var again, 
+ between a lot of codes.... then nobody knows where is the problem...
+ 'Cause javascript will not give you a tip there are two variables...
+ 2022.01.23(Sun) _Nam-Seok's_Study_Diary
+
+ I'll BE A FULL_STACK DEVELOPER IN 5 YEARS from now on....
+
+ ======================================================================
+ */
